4f03d83aaf25423d4a1dd440589c460efb600556
DESTA ATIKANANDA <d.atikananda.ft21@mail.umy.ac.id>
2022-12-09 15:48:19 +0700

    add int size public stack

--- Exercise4/Program.cs
+++ Exercise4/Program.cs
@@ -2,5 +2,17 @@
 
 namespace Exercise4
 {
-
+    class Stack
+    {
+        private string[] desta;
+        private int top;
+        private int max;
+        
+        public Stack(int size)
+        {
+            desta = new string[22];
+            top = -1;
+            max = 22;
+        }
+    }
 }
